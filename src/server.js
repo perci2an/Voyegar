1dbd9eadeda362e8933b957b78fd4ee5f87cc9da
perci2an <122770473+perci2an@users.noreply.github.com>
2023-09-28 17:11:39 +0900

    Middleware

--- src/server.js
+++ src/server.js
@@ -1,11 +1,22 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 5000;
 
 const app = express();
+const logger = morgan("dev");
+
+const privateMiddleware = (req, res, next) => {
+  const url = req.url;
+  if (url === "/protected") {
+    return res.send("<h1>Not Allowed</h1>");
+  }
+  console.log("Allowed, you may continue. ");
+  next();
+};
 
 const FirstHome = (tomato, potato) => {
-  return potato.send("<html><body><h1>끝까지</h1></body></html>");
+  return potato.send("<html><body><h1>가자</h1></body></html>");
 };
 
 const SecondHome = (spicy, noodle) => {
@@ -20,9 +31,11 @@ const FourthHome = (footBall, basketBall) => {
   return basketBall.send("<html><body><h1>없어</h1></body></html>");
 };
 
+app.use(logger);
+app.use(privateMiddleware);
 app.get("/", FirstHome);
 app.get("/about.html", SecondHome);
-app.get("/contact.html", ThirdHome);
+app.get("/protected.html", ThirdHome);
 app.get("/login.html", FourthHome);
 
 const handleListening = () => {
