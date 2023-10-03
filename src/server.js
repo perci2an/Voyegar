1604142a0d774eaf82a1c37e1fc36e9230b5c400
perci2an <122770473+perci2an@users.noreply.github.com>
2023-10-03 12:48:21 +0900

    pugApplications

--- src/server.js
+++ src/server.js
@@ -1,42 +1,20 @@
 import express from "express";
 import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import videoRouter from "./routers/videoRouter";
+import userRouter from "./routers/userRouter";
 
 const PORT = 5000;
 
 const app = express();
 const logger = morgan("dev");
-
-const privateMiddleware = (req, res, next) => {
-  const url = req.url;
-  if (url === "/protected") {
-    return res.send("<h1>Not Allowed</h1>");
-  }
-  console.log("Allowed, you may continue. ");
-  next();
-};
-
-const FirstHome = (tomato, potato) => {
-  return potato.send("<html><body><h1>가자</h1></body></html>");
-};
-
-const SecondHome = (spicy, noodle) => {
-  return noodle.send("<html><body><h1>해내는 거야</h1></body></html>");
-};
-
-const ThirdHome = (love, you) => {
-  return you.send("<html><body><h1>포기는</h1></body></html>");
-};
-
-const FourthHome = (footBall, basketBall) => {
-  return basketBall.send("<html><body><h1>없어</h1></body></html>");
-};
-
 app.use(logger);
-app.use(privateMiddleware);
-app.get("/", FirstHome);
-app.get("/about.html", SecondHome);
-app.get("/protected.html", ThirdHome);
-app.get("/login.html", FourthHome);
+
+app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views");
+app.use("/", globalRouter);
+app.use("/user", userRouter);
+app.use("/video", videoRouter);
 
 const handleListening = () => {
   console.log(`server on port http://localhost:${PORT}`);
