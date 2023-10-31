5be7a32ac0f70746771a4f6b41943624728989dd
perci2an <122770473+perci2an@users.noreply.github.com>
2023-10-31 21:24:05 +0900

    라우터 실습

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,67 @@
-export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
-export const see = (req, res) => res.render("watch");
-export const edit = (req, res) => res.render("edit");
-export const search = (req, res) => res.send("Search");
-export const upload = (req, res) => res.send("Upload");
-export const deleteVideo = (req, res) => res.send("Delete");
+let videos = [
+  {
+    title: "First Video",
+    rating: 5,
+    comments: 3495,
+    createdAt: "2 minutes ago",
+    views: 983423,
+    id: 1,
+  },
+  {
+    title: "Second Video",
+    rating: 5,
+    comments: 3495,
+    createdAt: "2 minutes ago",
+    views: 983423,
+    id: 2,
+  },
+  {
+    title: "Third Video",
+    rating: 5,
+    comments: 3495,
+    createdAt: "2 minutes ago",
+    views: 983423,
+    id: 3,
+  },
+];
+
+export const trending = (req, res) => {
+  return res.render("home", { pageTitle: "Home", videos });
+};
+
+export const watch = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  return res.render("watch", { pageTitle: `Watching: ${video.title}`, video });
+};
+
+export const getEdit = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  return res.render("edit", { pageTitle: `Editing: ${video.title}`, video });
+};
+
+export const postEdit = (req, res) => {
+  const { id } = req.params;
+  const { title } = req.body;
+  videos[id - 1].title = title;
+  return res.redirect(`/video/${id}`);
+};
+
+export const getUpload = (req, res) => {
+  return res.render("upload", { pageTitle: "Upload Video" });
+};
+
+export const postUpload = (req, res) => {
+  const { title } = req.body;
+  const newVideo = {
+    title,
+    rating: 1,
+    comments: 7777,
+    createdAt: "just now",
+    views: 23423,
+    id: videos.length + 1,
+  };
+  videos.push(newVideo);
+  return res.redirect("/");
+};
--- src/server.js
+++ src/server.js
@@ -12,6 +12,7 @@ app.use(logger);
 
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
+app.use(express.urlencoded({ extended: true }));
 app.use("/", globalRouter);
 app.use("/user", userRouter);
 app.use("/video", videoRouter);
