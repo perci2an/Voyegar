3a2cdb9c001c55915037d72b055ab66f0d011099
perci2an <122770473+perci2an@users.noreply.github.com>
2024-05-13 18:50:45 +0900

    쿠키, 세션을 이용한 인증

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 import "./db";
 import express from "express";
 import morgan from "morgan";
+import session from "express-session";
 import rootRouter from "./routers/rootRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
+import { localsMiddleware } from "./middleware";
 
 const app = express();
 const logger = morgan("dev");
@@ -12,6 +14,23 @@ app.use(logger);
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  session({
+    secret: "Hello!",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+app.use((req, res, next) => {
+  req.sessionStore.all((error, sessions) => {
+    console.log(sessions);
+    next();
+  });
+});
+
+app.use(localsMiddleware);
 app.use("/", rootRouter);
 app.use("/user", userRouter);
 app.use("/video", videoRouter);
