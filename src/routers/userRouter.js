48eabd247286cd533ab78224dccb04f1e6225e8f
perci2an <122770473+perci2an@users.noreply.github.com>
2024-05-18 16:41:09 +0900

    타이틀 수정

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,11 +1,18 @@
 import express from "express";
-import { edit, remove, logout, see } from "../controllers/userController";
+import {
+  edit,
+  remove,
+  logout,
+  see,
+  startGithubLogin,
+} from "../controllers/userController";
 
 const userRouter = express.Router();
 
 userRouter.get("/logout", logout);
 userRouter.get("/edit", edit);
 userRouter.get("/delete", remove);
+userRouter.get("/github/start", startGithubLogin);
 userRouter.get("/:id", see);
 
 export default userRouter;
